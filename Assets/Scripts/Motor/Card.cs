d4a186e31d31200d488a7b1657318da78eed8cf2
Gabriel Depotte <depott_g@epita.fr>
2019-07-19 09:49:53 +0200

    [motor] Added blank GameManager and Card classes

---
+++ Assets/Scripts/Motor/Card.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    public string moveName;
+    public List<string> moves;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
