5ae6151fa3eb1c3e64c74b0a20084f38a7936990
Gabriel Depotte <depott_g@epita.fr>
2019-07-19 11:17:39 +0200

    [motor] Made the random card picking, but a card can be picked twice

--- Assets/Scripts/Motor/GameManager.cs
+++ Assets/Scripts/Motor/GameManager.cs
@@ -15,11 +15,44 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        InitCards();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    private void InitCards()
+    {
+        int[] chosenCardsIndexes = new int[5];
+        for (int i = 0; i < 5; i++)
+            chosenCardsIndexes[i] = -1;
+
+        for (int i = 0; i < 5; i++)
+        {
+            int cardIndex = Random.Range(0, cardPrefabs.Count);
+            chosenCardsIndexes[i] = cardIndex;
+        }
+
+        Vector3 pos = cardA1Location.transform.position;
+        Destroy(cardA1Location);
+        Instantiate(cardPrefabs[chosenCardsIndexes[0]], pos, new Quaternion());
+
+        pos = cardA2Location.transform.position;
+        Destroy(cardA2Location);
+        Instantiate(cardPrefabs[chosenCardsIndexes[1]], pos, new Quaternion());
+
+        pos = cardB1Location.transform.position;
+        Destroy(cardB1Location);
+        Instantiate(cardPrefabs[chosenCardsIndexes[2]], pos, new Quaternion());
+
+        pos = cardB2Location.transform.position;
+        Destroy(cardB2Location);
+        Instantiate(cardPrefabs[chosenCardsIndexes[3]], pos, new Quaternion());
+
+        pos = freeCardLocation.transform.position;
+        Destroy(freeCardLocation);
+        Instantiate(cardPrefabs[chosenCardsIndexes[4]], pos, new Quaternion());
     }
 }
